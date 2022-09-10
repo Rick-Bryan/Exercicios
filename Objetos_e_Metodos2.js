ee93a48be131b2258437017325145f6fb0522b5d
Rick Bryan <88159010+Rick-Bryan@users.noreply.github.com>
2022-09-10 12:24:04 -0300

    Create Objetos_e_Metodos2.js

---
+++ Objetos_e_Metodos2.js
@@ -0,0 +1,154 @@
+/*let user = {
+  name: "Rick",
+  age: 19,
+  email: "rickbryanbatistalima@gmail.com",
+  city: "Belém",
+  blogPosts: ["Empadão de frango", "4 Receitas de purê de batata"],
+  login: function(){
+    console.log(`Logado como ${user.name}`)
+  },
+  logout: function(){
+    console.log(`Você deslogou de ${user.name}`)
+  }
+}
+user.login();
+user.logout();
+
+
+
+app.js
+No início deste ano
+24 De Jun.
+Desconhecido fez o upload de 1 item
+JavaScript
+app.js*/
+
+/*
+  01
+
+  - Crie um objeto "cat" que possui as seguintes propriedades:
+    - name, que recebe o nome do gato;
+    - age, que recebe a idade;
+    - color, que recebe a cor;
+    - bestFriends, que recebe um array com os melhores amigos do gato;
+    - sound, que é um método que retorna o miado do gato.
+  - Exiba o objeto no console.
+*/
+const cat = {
+  name: "Isis",
+  age: 4,
+  color: "Cinza",
+  bestFriends: ["Rick","Derick"],
+  sound: function(){
+    return "Miaaaaau"
+  }
+}
+//console.log(cat)
+/*
+  02
+
+  - Exiba a mensagem abaixo no console, substituindo os "X" pelas informações  
+    corretas.
+
+  Até aqui, o objeto "cat" possui as seguintes propriedades e valores: "name", que recebeu "X", "age", que recebeu "X", "color", que recebeu "X", "bestFriends", que recebeu um array com os itens "X" e "X", e "sound", que recebeu uma função que retorna "X".
+*/
+//console.log(`Até aqui, o objeto "cat" possui as seguintes propriedades e valores: "name", que recebeu "${cat.name}", "age", que recebeu "${cat.age}", "color", que recebeu "${cat.color}", "bestFriends", que recebeu um array com os itens "${cat.bestFriends[0]}" e "${cat.bestFriends[1]}", e "sound", que recebeu uma função que retorna "${cat.sound()}".`)
+/*
+  03
+
+  - Adicione 2 anos à idade do gato e exiba a idade atualizada no console.
+*/
+cat.age += 2
+//console.log(cat.age)
+
+/*
+  04
+
+  - Crie uma função que adiciona um amigo(a) no array de amigos do gato;
+  - Exiba o array de amigos no console para verificar se o novo amigo(a) foi  
+    adicionado.
+*/
+const addAmigoDoGato = array => array.push("Milton");
+  
+
+//addAmigoDoGato(cat.bestFriends)
+//console.log(cat.bestFriends)
+/*
+  05
+
+  - Crie uma função que adiciona mais uma cor à cor do gato;
+  - Utilize a sintaxe de colchetes para fazer isso;
+  - Exiba a nova cor do gato no console, também utilizando a sintaxe de  
+    colchetes.
+*/
+/*const addCorNoGato = (objeto,color) => {
+  const corNova = "Preto"
+  objeto+=color;   
+
+}
+const corNova = "Preto"
+addCorNoGato(cat.color,corNova)
+console.log(cat.color)*/
+  
+
+/*
+  06
+
+  - Crie uma função que retorna um boolean indicando se um valor recebido por  
+    parâmetro é um objeto;
+  - Utilize a função para exibir no console se "cat" é um objeto.
+*/
+const verifyIsObject = isObject=> typeof isObject === "object"
+ 
+console.log(verifyIsObject(cat))
+/*
+  07
+
+  - Crie um objeto "dog" que possui as mesmas propriedades do objeto "cat";
+  - Crie uma função que retorna a mensagem abaixo, com as informações corretas;
+  - Exiba a mensagem no console.
+
+  "A soma das idades de NOME_DO_GATO e NOME_DO_CACHORRO é RESULTADO_DA_SOMA."
+*/
+const dog = {
+  name: "Doguinho caramelo",
+  age: 4,
+  color: "Cinza",
+  bestFriends: ["Rick","Derick"],
+}
+const sumPets = (catName,dogName)=>{
+  const sumAgePet = catName + dogName
+  return `A soma das idades de ${catName} e ${dogName} é ${sumAgePet}.`
+}
+console.log(sumPets(cat.nam,dog.name)) // falta fazer
+/*
+  08
+
+  - Se você descomentar os 2 console.log abaixo, verá que a função "isAnSUV"  
+    está funcionando corretamente. Mas ela pode ser melhorada;
+  - Como você refatoraria esta função?
+*/
+
+const isAnSUV = car => {
+  if (car === 'Honda HR-V' || car === 'Jeep Renegade' || car === 'Ford EcoSport' || car === 'Hyundai iX35') {
+    return true
+  }
+
+  return false
+}
+
+// console.log(isAnSUV('Honda Civic'))
+// console.log(isAnSUV('Ford EcoSport'))
+
+/*
+  09
+
+  - Crie uma função que recebe um parâmetro 'type';
+  - Dentro da função, crie um objeto com as seguintes propriedades:
+    - null, que recebe a mensagem 'Seta, explicitamente, uma variável sem valor.'
+    - undefined, que recebe a mensagem 'Representa um valor não-setado.'
+    - object, que recebe a mensagem 'Arrays, Datas, Objetos literais, Funções, etc.'
+  - Se o valor que o parâmetro type receber corresponder à alguma das 3  
+    propriedades, retorne a mensagem que a propriedade armazena;
+  - Teste a função, exibindo no console a mensagem de cada propriedade.
+*/
