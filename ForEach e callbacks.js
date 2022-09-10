cde06a836a93aba0c729f42ca4938af733a4576a
Rick Bryan <88159010+Rick-Bryan@users.noreply.github.com>
2022-09-10 12:19:57 -0300

    Create ForEach e callbacks.js

---
+++ ForEach e callbacks.js
@@ -0,0 +1,381 @@
+/*const socialNetworks = ["Instagram","Twitter","Facebook","Youtube"]
+const logArrayInfo = (socialNetwork,index,array)=>{
+  console.log(index,socialNetwork,array)
+}
+socialNetworks.forEach(logArrayInfo)*/
+/*const ul = document.querySelector('[data-js="ul"]')
+const socialNetworks = ["Instagram","Twitter","Facebook","Youtube"]
+
+let htmlTemplate= ''
+socialNetworks.forEach(socialNetwork=>{
+  htmlTemplate += `<li>${socialNetwork}</li>`
+})
+ul.innerHTML = htmlTemplate
+*/
+
+
+/*
+  Dica:
+
+  - Alguns exercícios utilizarão métodos, propriedades e funcionalidades vistos  
+    no começo do curso. Se for necessário, reveja partes das aulas anteriores  
+    para relembrar.
+*/
+
+/*
+  01
+
+  - Converta a função abaixo em uma arrow function e utilize-a para exibir um  
+    valor no console.
+*/
+
+const convertToString = value => String(value)
+  
+//console.log(convertToString(14))
+
+/*
+  02
+
+  - Crie uma função que retorne a quantidade de caracteres que uma string  
+    recebida por parâmetro possui.
+*/
+/*let countCaracteres = 0
+const caracter= (valor)=>{
+  for(let i= 0; i<valor.length;i++){
+    countCaracteres ++
+  }
+  console.log(countCaracteres)
+}
+caracter("Telefone")*/
+
+//Resolução do curso muito melhor kk
+const getStringLength = string => string.length
+
+//console.log(getStringLength("Olá"))
+/*
+
+  03
+
+  - Crie uma função que retorne todos os caracteres de uma string em letras  
+    minúsculas;  
+  - Utilize a função para exibir a string abaixo no console.
+
+  "CHOCOTONE E OVO DE PÁSCOA JUNTOS NO MERCADO EM PLENO FEVEREIRO"
+*/
+const allToLowerCase = (valor)=>{
+  return valor.toLowerCase()
+}
+const lower = allToLowerCase("BOM DIA")
+//console.log(lower)
+//Resolução do curso  bem melhor
+const convertToLowerCase = string => string.toLowerCase();
+
+//console.log(convertToLowerCase("CHOCOTONE E OVO DE PÁSCOA JUNTOS NO MERCADO EM PLENO FEVEREIRO"))
+/*
+  04
+
+  - Crie uma função que recebe 2 parâmetros: um caractere e uma string;
+  - Ao ser invocada, a função deve retornar o index do caractere na string.
+*/
+/*const myFunc = (caractere, string) => {
+  return string.indexOf(`${caractere}`)
+}
+console.log(myFunc("i", "Bom Dia"))*/
+
+//Resolução do curso
+
+const getIndex= (character,string) => string.indexOf(character)
+
+//console.log(getIndex("O", "Oi"))
+/*
+  05
+
+  - Crie uma função que, ao ser invocada, retorna um boolean indicando se o item  
+    passado por argumento existe no array (também passado por argumento).
+*/
+/*let bol = false
+const verifyArguments = (item,array) =>{
+  if(array.includes(item)){
+    bol = true
+  }
+  return bol 
+  
+}
+const arrayVerify= ["Macaco","Onça","Leão","cutia"]
+*/
+//console.log(verifyArguments("Onça", arrayVerify))
+
+const arrayVerify= ["Macaco","Onça","Leão","cutia"]
+let verify = false
+const verifyArguments = (arrayVerify,arrayitems) =>{
+  arrayVerify.forEach((item)=>{
+    if (arrayitems.includes(item)){
+      verify =true
+    }
+  })
+ return verify 
+}
+//console.log(verifyArguments(arrayVerify,"elefante"))
+
+//Resolução do curso bem melhor kk mesmo resultado em apenas 1 linha
+
+const isItemIncluded = (item, array) => array.includes(item)
+
+//console.log(isItemIncluded(2,[7,3,1]))
+/*
+  06
+
+  - Crie uma função que retorna a concatenação de 2 arrays, passados como  
+    argumentos em sua invocação;
+*/
+/*const concatFunc = (array1,array2) =>{
+  return array1.concat(array2)
+}
+const firstArray = ["O","Dia","é","Bonito"]
+const secondArray = ["A","Noite","é","Bonita"]
+const result = concatFunc(firstArray,secondArray)
+console.log(result)
+*/
+
+//Resolução do curso
+
+const concatArray = (firstArray,secondArray) => firstArray.concat(secondArray)
+
+//console.log(concatArray([1,2,3],[4,5,6]))
+/*
+  07
+
+  - Crie uma função que retorna o array passado como argumento em sua invocação,  
+    mas com o último item removido.
+*/
+/*const removeLastItem= (array) => {
+  array.pop()
+  const newArray = array
+  return newArray
+}
+const arrayLastItem = ["A","Dona","Aranha","Subiu","Pela","Parede"]
+const resultado = removeLastItem(arrayLastItem)*/
+//console.log(resultado)
+
+//Resolução do curso 
+/*
+const removeLastItem = array => {
+  array.pop()
+  return array
+}*/
+//console.log(removeLastItem([1,2,4,5]))
+/*
+  08
+
+  - Crie uma função que retorna se o valor passado como argumento em sua  
+    invocação é null.
+*/
+/*
+const verifyNull =(valor)=>{
+  return valor === null
+}
+const functionResult = verifyNull(null)
+console.log(functionResult)
+*/
+//Resolução do curso
+const isNull = value => value === null
+//console.log(isNull(null))
+/*
+  09
+
+  - Crie uma função que apenas invoca uma função de callback recebida por  
+    parâmetro;
+  - Crie outra função que apenas exibe seu nome no console;
+  - Invoque a função que recebe um callback por parâmetro, passando como  
+    argumento a função que exibe seu nome no console e veja se o nome realmente  
+    foi exibido.
+*/
+
+const showName = (name)=>{
+  
+  console.log("Rick")
+}
+const invocaCallback = (callback) => {
+  callback()
+}
+//invocaCallback(showName())
+/*
+  10
+
+  - Crie uma função que invoca uma função de callback recebida por parâmetro.  
+    A invocação da função recebida por parâmetro deve receber um valor como  
+    argumento;
+  - Crie uma função que retorna o triplo de um número recebido por parâmetro;
+  - Faça com que a invocação da função descrita no 1º item deste exercício (10)  
+    resulte no triplo de 33.
+*/
+const funcaoCallback = (value,callback)=>{
+  return callback(value)
+}
+const tripleNumber = (number) =>{
+  return number*3;
+}
+//console.log(funcaoCallback(10,tripleNumber)) // falta fazer*/
+
+//resolução do curso
+
+const callCallBack = (value,callback) => callback(value);
+
+const triple = (value) => value * 3;
+//console.log(callCallBack(33,triple))
+
+/*
+  11
+
+  - Utilizando um forEach, baseado no array "numbers", a cada iteração, exiba a  
+    mensagem abaixo no console, substituindo os "X" pelas informações corretas;
+  
+  "O Xº item do array [X, X, X] é X."
+*/
+
+const numbers = [1, 2, 3]
+/*
+numbers.forEach((number,index,array)=>{
+  console.log(`O ${index+1}º item do array ${array} é ${number}.`)
+})
+*/
+//Resolução do curso 
+const showNumbersInfo = (item,index,array) =>{
+  const itemPosition = index + 1
+  const items = array.join(', ')
+  
+  console.log(`O ${itemPosition}º item do array [${items}] é ${item}.`)
+}
+
+//numbers.forEach(showNumbersInfo)
+
+/*
+  12
+
+  - Converta o for loop abaixo em um forEach;
+  - Após a conversão, verifique se a cópia do array lettersCopy realmente foi  
+    criada.
+*/
+
+const letters = ['v', 'e', 'p']
+let lettersCopy = []
+
+/*for (let i = 0; i < letters.length; i++) {
+  lettersCopy.push(letters[i])
+}*/
+/*letters.forEach((item)=>{
+  lettersCopy.push(item)
+})
+console.log(lettersCopy)
+*/
+//Resolução do curso
+
+letters.forEach(letter=>{
+  lettersCopy.push(letter)
+})
+/*
+  13
+
+  - Inclua o markup abaixo em seu index.html;
+  - Gere um template HTML com parágrafos. Cada parágrafo deve conter um item do  
+    array "review";
+  - Ao gerar o template, verifique no browser se os parágrafos foram incluídos  
+    dentro da section vazia do markup abaixo.
+
+  <article>
+    <header>
+      <h1>Sobre "Jurassic Park"</h1>
+    </header>
+
+    <section data-js="section"></section>
+  </article>
+*/
+
+const section = document.querySelector('[data-js="section"]')
+
+const review = [
+  'Eu sempre adorei o filme e quando descobri que tinha o livro também fiquei doido. Demorei um pouco mas acabei comprando e finalmente li \o/.',
+  'O primeiro filme foi baseado nesse livro, porém o livro (como sempre) é muito mais completo, com mais personagens, mais acontecimentos e até mesmo mais dinossauros. Na verdade nesse livro tem coisas do segundo e terceiro filme também, eles mudaram bastante nos filmes, acho que pra ficar mais comercial, e se o filme é bom, o livro é 100 vezes melhor.',
+  'Michael é um ótimo autor, esse sim pesquisa muito antes de escrever um livro, além da história que já prende sua atenção, ele fala bastante de genética (pra explicar como os dinossauros foram criados) e acaba falando um pouco de programação (informática), por causa dos programas avançados e modernos que o parque tinha. E isso foi uma das coisas que eu achei muito legal, ele explica as coisas com gráficos, tabelas, códigos ... enfim, o cara é foda hahaha.',
+  'Recomendo esse livro pra quem curte uma boa história de ficção. Apesar de muita gente pensar que o livro não tem graça, porque o legal mesmo é ver o dinossauro no filme, com todos os efeitos especiais, eu digo pra deixar esse pensamento de lado, pois a história é tão bem contada e os detalhes são tão bem relatados, que você passa a fazer parte da história, e vive todas as emoções hahaha.'
+]
+
+/*let paragraphs = ''
+review.forEach((item)=>{
+  paragraphs += `<p>${item}</p>`
+})
+*/
+//Resolução do curso mesma coisa
+let paragraphs = ''
+const createParagraphs = (paragraph)=>{
+  paragraphs += `<p>${paragraph}</p>`
+}
+review.forEach(createParagraphs)
+
+section.innerHTML = paragraphs
+
+/*
+  14
+
+  - Implemente uma função que retorna uma string com a quantidade de pessoas que  
+    curtiram um post, conforme descrito a seguir;
+  - A função deve receber por parâmetro um array com os nomes das pessoas que  
+    curtiram o post/vídeo/foto;  
+  - Se o array recebido estiver vazio, a mensagem que a função deve retornar é  
+    "Ninguém curtiu isso";  
+  - Se o array conter apenas um nome, como "Rafael", por exemplo, a mensagem  
+    retornada deve ser "Rafael curtiu isso";  
+  - Se o array conter 2 nomes, a mensagem retornada deve ser  
+    "NOME_1 e NOME_2 curtiram isso";  
+  - Se o array conter 3 nomes, a mensagem retornada deve ser  
+    "NOME_1, NOME_2 e NOME_3 curtiram isso";  
+  - Se o array conter 4 ou mais nomes, a mensagem retornada deve ser  
+    "NOME_1, NOME_2 e mais X pessoas curtiram isso". O "X" deve ser substituído  
+    pelo restante da quantidade de pessoas que curtiram o post (além das duas  
+    pessoas já mencionadas no início da mensagem).
+*/
+/*const names = [""] 
+
+const curtidas = (array)=>{ // nao consegui fazer 
+  for(let i= 0;i<array.length;i++){
+    if (array.length===1){
+      return `${array[i]} curtiu isso`
+    }
+    else if(array.length ===2){
+      return `${array[i]} e ${array[i+1]} curtiu isso`
+    }
+    else if(array.length ===3){
+      return `${array[i]} , ${array[i+1]} e ${array[i+2]} curtiu isso`
+    }
+    else if(array.length >=4){
+      return `${array[i]} e ${array[i+1]} e mais ${array.length} pessoas curtiram isso`
+    }
+    else if (array.length < 1){
+      return `Ninguém curtiu isso`
+    }
+  }
+}*/
+//console.log(curtidas(names))
+
+//Resolução do curso 
+
+const getLikeMessage = (names = [])=>{
+  const firstName = names[0];
+  const secondName = names[1];
+  const thirdName = names[2];
+  const totalNamesMinusTwo = names.length - 2 
+  
+  switch(names.length){
+    case 0:
+      return `Ninguém curtiu isso`
+    case 1:
+      return `${firstName} curtiu isso`
+    case 2 :
+      return `${firstName} e ${secondName} curtiram isso`
+    case 3 :
+      return `${firstName}, ${secondName} e ${thirdName} curtiram isso`
+    default:
+      return  `${[firstName]} e ${secondName} e mais ${totalNamesMinusTwo} pessoas curtiram isso`
+  }
+}
+console.log(getLikeMessage(["Rick","Bryan","Lima","Batista"]))
